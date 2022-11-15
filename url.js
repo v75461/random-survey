cc2133c21bb7641faf37a0b1acfc8df90b779eb5
v75461 <118248421+v75461@users.noreply.github.com>
2022-11-15 14:46:35 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/xg2mngBJqf6se3reA',
+	'https://forms.gle/Sx1YanLLtN3QuYMS9'
 ];
